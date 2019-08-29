74419868720179af4fb82712f7d77271b2099c80
Icaro Rodolpho <mythirfm@gmail.com>
2019-08-29 11:06:05 -0300

    resto

--- resources/views/validar.blade.php
+++ resources/views/validar.blade.php
@@ -12,20 +12,7 @@
         <!-- Styles -->
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
-        <script
-                src="https://code.jquery.com/jquery-3.4.1.min.js"
-                ></script>
-        <script>
-            const Url = 'http://aliancaemp.ddns.net:50001/api/syclus/sistema/certificado/{{$uid}}';
-            const Data = fetch(Url)
-                .then(data=>{return data.json()})
-                .then(res=>{console.log(res)})
 
-            $('json').value = Data;
-
-
-
-        </script>
 
 
 
@@ -85,53 +72,45 @@
     <body>
 
 
-    <form>
-        @csrf
-        <input href="validar" name="json" value="" id="json">
-        <input type="submit">
+        <div class="position-ref full-height col-lg-12">
+            <div class="flex-center">
+                <img style="" src="/img/logo.png" width="200" height="120" alt="logo" title="logo"><br><br>
+            </div>
+            <div class="col-md-12">
+                <br><br><br><br><br>
+                <p class="col-md-12 font-weight-bold" style="position:relative;"> Certificado {{$certificado->idCertificado}} validado eletronicamente as {{\Carbon\Carbon::now('America/Maceio')->format('H:i:s')}} do dia {{\Carbon\Carbon::now('America/Maceio')->format('d/m/Y')}}
+                para a empresa x relativo aos descartes abaixo:
+                </p>
+            </div>
 
-    </form>
+            <div class="col-md-12">
+                <table class="col-md-12" >
+                    <tr  style="border: 1px solid">
+                        <th style="min-width: 100px;">Empresa</th>
+                        <th>Data do descarte</th>
+                        <th>Manifesto</th>
+                        <th>Endereço</th>
+                        <th>Quantidade</th>
+                    </tr>
+                    @foreach($coletas->items as $coleta)
+                    <tr style="border-bottom: 1px solid">
+                        <td>{{$coleta["transportador"]["nomeRazaoSocial"]}}</td>
+                        <td>{{(new \Carbon\Carbon($coleta["dataEmissao"]))->format('d-m-Y')}}</td>
+                        <td>{{$coleta["numeroTransportador"]}}</td>
+                        <td>{{$coleta["obra"]["descricao"]}}</td>
+                        <td>@foreach((object)$coleta["itens"]["items"] as $d)
+                                <p>{{$d["descricao"]}} : {{$d["qtde"]}}m³</p>
+                            @endforeach
+                        </td>
 
-
-{{--        <div class="position-ref full-height col-lg-12">--}}
-{{--            <div class="flex-center">--}}
-{{--                <img style="" src="/img/logo.png" width="200" height="120" alt="logo" title="logo"><br><br>--}}
-{{--            </div>--}}
-{{--            <div class="col-md-12">--}}
-{{--                <br><br><br><br><br>--}}
-{{--                <p class="col-md-12 font-weight-bold" style="position:relative;"> Certificado {{$certificado->idCertificado}} validado eletronicamente as {{\Carbon\Carbon::now('America/Maceio')->format('H:i:s')}} do dia {{\Carbon\Carbon::now('America/Maceio')->format('d/m/Y')}}--}}
-{{--                para a empresa x relativo aos descartes abaixo:--}}
-{{--                </p>--}}
-{{--            </div>--}}
-
-{{--            <div class="col-md-12">--}}
-{{--                <table class="col-md-12" >--}}
-{{--                    <tr  style="border: 1px solid">--}}
-{{--                        <th style="min-width: 100px;">Empresa</th>--}}
-{{--                        <th>Data do descarte</th>--}}
-{{--                        <th>Manifesto</th>--}}
-{{--                        <th>Endereço</th>--}}
-{{--                        <th>Quantidade</th>--}}
-{{--                    </tr>--}}
-{{--                    @foreach($coletas->items as $coleta)--}}
-{{--                    <tr style="border-bottom: 1px solid">--}}
-{{--                        <td>{{$coleta["transportador"]["nomeRazaoSocial"]}}</td>--}}
-{{--                        <td>{{(new \Carbon\Carbon($coleta["dataEmissao"]))->format('d-m-Y')}}</td>--}}
-{{--                        <td>{{$coleta["numeroTransportador"]}}</td>--}}
-{{--                        <td>{{$coleta["obra"]["descricao"]}}</td>--}}
-{{--                        <td>@foreach((object)$coleta["itens"]["items"] as $d)--}}
-{{--                                <p>{{$d["descricao"]}} : {{$d["qtde"]}}m³</p>--}}
-{{--                            @endforeach--}}
-{{--                        </td>--}}
-
-{{--                    </tr>--}}
-{{--                    @endforeach--}}
-{{--                </table>--}}
-{{--            </div>--}}
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
 
 
 
 
-{{--        </div>--}}
+        </div>
     </body>
 </html>
